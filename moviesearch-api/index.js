7de80a942e7c0cb0f61431211e53ac73a766599a
Jonathan Vazquez <vamtha24@gmail.com>
2018-07-18 21:48:51 -0500

    Add initial routes

--- moviesearch-api/index.js
+++ moviesearch-api/index.js
@@ -1,5 +1,16 @@
 const { send } = require('micro');
+const { router, get } = require('microrouter');
+const fetch = require('node-fetch');
 
-module.exports = async (req, res) => {
-  send(res, 200, { data: []});
+async function getMovies(req, res) {
+  send(res, 200, {});
 }
+
+const info = (req, res) => send(res, 200, 'Movie Search Api Service');
+const notfound = (req, res) => send(res, 404, 'Not found Route');
+
+module.exports = router(
+  get('/api/imdb/search/:title', getMovies),
+  get('/', info),
+  get('*', notfound)
+);
